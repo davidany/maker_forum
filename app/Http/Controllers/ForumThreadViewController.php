524eac18d63ce10bbbb9e56fec70f8425a938d52
david abraham <dave@springbasemedia.com>
2024-11-05 01:05:19 -0500

    added models migrations and controllers with artisan
    php artisan make:model ForumCategory  -mcfs
    php artisan make:model ForumThread -mcfs
    php artisan make:model ForumPost -mcfs
    php artisan make:model ForumThreadView -mcfs

---
+++ app/Http/Controllers/ForumThreadViewController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ForumThreadViewController extends Controller
+{
+    //
+}
---
+++ app/Models/ForumThread.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ForumThread extends Model
+{
+    /** @use HasFactory<\Database\Factories\ForumThreadFactory> */
+    use HasFactory;
+}
